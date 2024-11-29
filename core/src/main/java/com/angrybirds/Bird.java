0c40e025120e1aa298b6839d121161c1eacd8261
Gaurav <163036772+gauravchauhan0112@users.noreply.github.com>
2024-11-29 23:44:34 +0530

    Update Bird.java

--- core/src/main/java/com/angrybirds/Bird.java
+++ core/src/main/java/com/angrybirds/Bird.java
@@ -1,111 +1,28 @@
-
 package com.angrybirds;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.Array;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Rectangle;
+public abstract class BaseGameScreen implements Screen {
+    public AngryBirdsGame game;
+    public Array<Bird> birds;
+    public Array<BuildingBlock> blocks;
+    public Array<Pig> pigs;
 
-public class Bird {
-    protected Texture birdTexture;
-    protected Vector2 position;
-    protected Vector2 velocity;
-    protected float scale;
-     public boolean launched;
-    protected float power;
-    protected Rectangle bounds;
-    protected boolean isSpecial;
+    public Bird currentBird;
+    public int remainingBirds;
+    public int score;
 
-
-
-    public Bird(Texture texture, float x, float y, float scale, float power) {
-        this.birdTexture = texture;
-        this.position = new Vector2(x, y);
-        this.velocity = new Vector2(0, 0);
-        this.scale = scale;
-        this.power = power;
-        this.launched = false;
-        this.isSpecial = false;
-        newBounds();
+    public BaseGameScreen(AngryBirdsGame game) {
+        this.game = game;
+        this.birds = new Array<>();
+        this.blocks = new Array<>();
+        this.pigs = new Array<>();
     }
 
-    public void launch(Vector2 initialVelocity) {
-        this.velocity.set(initialVelocity);
-        this.launched = true;
-    }
-
-    public void update(float deltaTime) {
-        if (launched) {
-            velocity.y -= 980f * deltaTime;
-
-            position.x += velocity.x * deltaTime;
-            position.y += velocity.y * deltaTime;
-
-            velocity.x *= 0.99f;
-            velocity.y *= 0.99f;
-
-            newBounds();
-        }
-    }
-
-    protected void newBounds() {
-        float width = birdTexture.getWidth() * scale;
-        float height = birdTexture.getHeight() * scale;
-        if (bounds == null) {
-            bounds = new Rectangle(position.x, position.y, width, height);
-        } else {
-            bounds.set(position.x, position.y, width, height);
-        }
-    }
-
-    public void useSpecial() {
-        if (!isSpecial) {
-            isSpecial = true;
-        }
-    }
-
-
-    public Rectangle getBounds() {
-        return bounds;
-    }
-
-    public Vector2 getPosition() {
-        return position;
-    }
-
-    public void setPosition(float x, float y) {
-        position.set(x, y);
-        newBounds();
-    }
-
-    public void setLaunched(boolean launched) {
-        this.launched = launched;
-    }
-
-    public float getPower() {
-        return power;
-    }
-
-    public Texture getBirdTexture() {
-        return birdTexture;
-    }
-
-    public float getScale() {
-        return scale;
-    }
-
-    public void dispose() {
-        birdTexture.dispose();
-    }
-
-    public Vector2 getVelocity() {
-        return velocity;
-    }
-
-    public float getDamage() {
-        return velocity.x*0.001f;
-//        return 0;
-    }
-
+    // Abstract methods to ensure consistent implementation
+    public abstract void initializeLevel();
+    public abstract void saveGameState();
+    public abstract void loadGameState();
 }
+
 
